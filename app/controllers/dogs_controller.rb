9f70f2c66a177c5101a1876c3a6a011c027770f4
estherkimyunjung <estherkimyunjung@gmail.com>
2020-05-20 15:29:14 -0500

    pair lab with eric

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,17 @@
 class DogsController < ApplicationController
+
+    before_action :current_dog, only: :show
+
+    def index 
+        # byebug
+        @dogs = Dog.all
+    end
+
+    def show
+    end
+
+    def current_dog
+        @dog = Dog.find(params[:id])
+    end
+    
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,12 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :alias, :title, uniqueness: true
+
+    def get_name
+        self.first_name.capitalize + " " + self.last_name.capitalize
+    end
+
+    def title_exist?
+        
+    end
 end
